35be5f840e280e7b2df4d485a4554ebe7fccb90e
Ben Hsieh <bqh5026@Kevins-iMac.local>
2019-09-03 00:59:22 -0400

    Updates

---
+++ precache-manifest.a628079af5538bc66f15b2ab3a359a1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7b3f890c996e40d54e946b245883a61",
+    "url": "/Guide-to-Investing/index.html"
+  },
+  {
+    "revision": "ee1dddf88ed39cc92abd",
+    "url": "/Guide-to-Investing/static/css/main.2352e77f.chunk.css"
+  },
+  {
+    "revision": "a07b191e8ba3fafa4edf",
+    "url": "/Guide-to-Investing/static/js/2.408e06ed.chunk.js"
+  },
+  {
+    "revision": "ee1dddf88ed39cc92abd",
+    "url": "/Guide-to-Investing/static/js/main.71a91881.chunk.js"
+  },
+  {
+    "revision": "e9ca4063b63abc77ce4e",
+    "url": "/Guide-to-Investing/static/js/runtime~main.59d1a38b.js"
+  },
+  {
+    "revision": "d5ead8b37cef438524aacf63bcde6d67",
+    "url": "/Guide-to-Investing/static/media/dollar-cost-vs-lump-sum-investing-2.d5ead8b3.jpg"
+  }
+]);
