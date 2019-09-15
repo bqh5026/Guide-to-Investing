a1f25b194cd73f23a76dc9e45235b107c652a957
Ben Hsieh <bqh5026@Kevins-iMac.local>
2019-09-14 23:54:36 -0400

    Edit digital currency component page

--- src/components/digital_currencies.component.jsx
+++ src/components/digital_currencies.component.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 const DigitalCurrencies= () => (
     <div>
       <h1>Digital Currencies</h1>
+      <p><strong>Bitcoin</strong> has emerged as the leading digital currency in the market. Athough bitcoin was valued at less than $0.01 in May 2010, bitcoin value has risen to over $6,000 according to recent price data. Hence, digital currencies may 
+      be an interesting way to diversify an investing portfolio.</p>
     </div>
   );
 
