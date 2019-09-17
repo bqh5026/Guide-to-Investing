d6eee9399ca6074622bd66e586f88f34c7aa9262
Ben Hsieh <bqh5026@Kevins-iMac.local>
2019-09-16 22:14:01 -0400

    Add HashRouter to App.js and edit home.component.jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { HashRouter, Route, Link } from 'react-router-dom';
 
 import './App.css';
 
@@ -12,25 +12,38 @@ import Bonds from './components/bonds.component';
 
 import Investing from './images/dollar-cost-vs-lump-sum-investing-2.jpg'; 
 
-function App() {
-  return (
-    <div className="App">
-        <h1>Guide to Investing</h1>
-        <div>
-            <Switch>
-              <Route exact path="/" component={HomePage} />
-              <Route path="/real_estates" component={RealEstates} />
-              <Route path="/equities" component={Equities} />
-              <Route path="/digital_currencies" component={DigitalCurrencies} />
-              <Route path="/precious_metals" component={PreciousMetals} />
-              <Route path="/bonds" component={Bonds} />
-            </Switch>     
+class App extends Component {
+  render() {
+    return (
+      <HashRouter basename="/">
+        <div className="App">
+            <div>
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/real_estates">Real Estates</Link></li>
+                <li><Link to="/equities">Equities</Link></li>
+                <li><Link to="/digital_currencies">Digital Currencies</Link></li>
+                <li><Link to="/precious_metals">Precious Metals</Link></li>
+                <li><Link to="/bonds">Bonds</Link></li>
+              </ul>
+
+              <hr />
+
+                <Route exact path="/" component={HomePage} />
+                <Route path="/real_estates" component={RealEstates} />
+                <Route path="/equities" component={Equities} />
+                <Route path="/digital_currencies" component={DigitalCurrencies} />
+                <Route path="/precious_metals" component={PreciousMetals} />
+                <Route path="/bonds" component={Bonds} />
+                
+            </div>
+            <div className="image">
+                <img src={Investing} alt='Investing' />
+            </div>  
         </div>
-        <div className="image">
-            <img src={Investing} alt='Investing' />
-        </div>  
-    </div>
-  );
+      </HashRouter> 
+    );
+  }
 }
 
 export default App;
--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -2,51 +2,12 @@
 
 import React from 'react';
 
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-
-import RealEstates from '../components/real_estates.component.jsx'; 
-import Equities from '../components/Equities/equities.component';
-import DigitalCurrencies from '../components/digital_currencies.component';
-import PreciousMetals from '../components/precious_metals/precious_metals.component';
-import Bonds from '../components/bonds.component';
-
 import './homepage.styles.css'; 
 
-
-
 const HomePage = () => (
-  <Router>
-  <div>
-    <h1>Home Page</h1>
-      <div>
-         <ul>
-            <li>
-               <Link to="/real_estates">Real Estates</Link>
-            </li>
-            <li>
-               <Link to="/equities">Equities</Link>
-            </li>
-            <li>
-               <Link to="/digital_currencies">Digital Currencies</Link>
-            </li>
-            <li>
-               <Link to="/precious_metals">Precious Metals</Link>
-            </li>
-            <li>
-               <Link to="/bonds">Bonds</Link>
-            </li>
-         </ul>
-      </div>
-
-    <hr />
-
-    <Route path="/real_estates" component={RealEstates} />
-    <Route path="/equities" component={Equities} />
-    <Route path="/digital_currencies" component={DigitalCurrencies} />
-    <Route path="/precious_metals" component={PreciousMetals} />
-    <Route path="/bonds" component={Bonds} />
-  </div>
-  </Router>
+<div>
+   <h1>Guide to Investing</h1>
+</div>
 );
 
 export default HomePage;
