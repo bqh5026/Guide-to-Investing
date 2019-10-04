e23933dc7ef981e7ab179f442367fe064d11a534
Ben Hsieh <bqh5026@Kevins-iMac.local>
2019-10-03 22:50:29 -0400

    Edit REDME.md file; update digital currencies component and styles; deploy site to Heroku

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Guide to Investing
 
-* Corresponding site @ https://bqh5026.github.io/Guide-to-Investing/
+* Corresponding GitHub site @ https://bqh5026.github.io/Guide-to-Investing/
+* Corresponding Heroku site @ https://hidden-journey-03332.herokuapp.com/
--- src/components/digital_currencies/digital_currencies.component.jsx
+++ src/components/digital_currencies/digital_currencies.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import './digital_currencies.styles.css';
+import digitalCurrenciesModule from './digital_currencies.module.css';
 
 const DigitalCurrencies= () => (
-    <div>
+    <div className={digitalCurrenciesModule.digitalCurrencies}>
       <h1>Digital Currencies</h1>
       <p><strong>Bitcoin</strong> has emerged as the leading digital currency in the market. Athough bitcoin was valued at less than $0.01 in May 2010, bitcoin value has risen to over $6,000 according to recent price data. Hence, digital currencies may 
       be an interesting way to diversify an investing portfolio.</p>
