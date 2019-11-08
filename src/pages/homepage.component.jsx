b9f9aa56d14c7bb358e9c6d912724e078a5ae4a7
Ben Hsieh <bqh5026@gmail.com>
2019-11-08 00:09:54 -0500

    Add online brokers information to homepage.component.jsx

--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -14,6 +14,15 @@ const HomePage = () => (
    had passed regulations that made investing safer and not subject to frauds.
    So what are the options to invest in the market? The purpose of this website is to provide 
    an overview of the exciting investment world.</p>
+
+   <p>Before you start, you should have at least a regular online brokerage account. Some of the online brokers
+   that I recommend are Etrade, Firstrade, Robinhood, and TD Ameritrade. The trend in the industry is moving 
+   towards zero commision trading. Robinhood and Firstrade are the best online brokers that I know of at the 
+   moment. Etrade offers zero commision trading with certain limitations, but the Etrade platform is very 
+   robust. Robinhood is destined to be the next unicorn company in the stock market, so Robinhood offers a 
+   very robust platform as well. However, Robinhood may be more limited than Etrade with regard to investment 
+   options. Firstrade offers a good balance of everything in zero commision trading and wide variety of investment
+   options. The trading platfor is decent though not as sophisticated, in my opinion, as Etrade.</p>
 </div>
 );
 
